4edb1f3669c1ba5332bb094979c27963aadd97ae
Tasnimul009 <tasnimul008@gmail.com>
2025-01-11 16:24:11 +0600

    Create E - Check if a subtring is in the string or not.c

---
+++ E - Check if a subtring is in the string or not.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[200], subStr[200];
+    scanf("%s%s", str, subStr);
+    int ind, j=0,ct=0;
+    for(ind=0;ind<strlen(str);ind++){
+        if(str[ind]==subStr[j]){
+            j++;
+            if(subStr[j]=='\0'){
+                ct++;
+                j=0;
+            }
+        }
+        else{
+            j=0;
+        }
+    }
+    if(ct==0){
+        printf("not found");
+    }
+    else{
+        printf("found");
+    }
+}    
